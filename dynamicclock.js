02736672371f9b8c9b57cc87b79d0fff8162e957
Jackie Jr <jwysinger@gmail.com>
2023-05-26 10:34:46 -0500

    added dark toggle javascript code

--- dynamicclock.js
+++ dynamicclock.js
@@ -1,2 +1,9 @@
+
+// Initialize section and icon variables. First line selects section element, second line seles icon class. Uses CSS for selection
 let section = document.querySelector("section"),
-	icons = document.querySelector(".icons");
+		icons = document.querySelector(".icons");
+
+// icons.onclick assigns event handler function to onclick event, next line toggles the dark class on section element.  
+icons.onclick = () => {
+	section.classList.toggle("dark");
+}
