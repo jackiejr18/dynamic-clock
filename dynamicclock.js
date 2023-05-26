b14f4f9aa99cfc5ab5c0265716cbb39596ad7a1c
Jackie Jr <jwysinger@gmail.com>
2023-05-26 12:14:05 -0500

    added setInterval function in javascript

--- dynamicclock.js
+++ dynamicclock.js
@@ -7,3 +7,13 @@ let section = document.querySelector("section"),
 icons.onclick = () => {
 	section.classList.toggle("dark");
 }
+
+// setInterval is used as a callback function that is to be executed repeatedly
+setInterval(() => {
+	let date = new Date(), // creates a new Date object to get current date and time 
+	hour = date.getHours(), // store hour from date object
+	min = date.getMinutes(), // store minute from date object
+	sec = date.getSeconds(); // store seconds from date object 
+
+
+})
